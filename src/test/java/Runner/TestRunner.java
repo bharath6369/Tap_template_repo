e6eda1f287b7dafcc9f660c129eaf38b6ab16f0a
Bharath <109291386+bharath6369@users.noreply.github.com>
2023-07-24 12:39:44 +0530

    Update TestRunner.java

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,1 +1,16 @@
+package Runner;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/Features/POM.feature",
+        tags = "@POC",
+        glue = {"Steps"},
+        plugin = {"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+
+public class POMRunner {
+}
